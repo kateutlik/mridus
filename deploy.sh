2b68e3e63e17759ad5178720ba3f0afa7a99e6b2
Katherina Utlik <katherinautlik@gmail.com>
2016-07-04 22:12:13 +0300

    aded heroku in travis configuration

--- deploy.sh
+++ deploy.sh
@@ -16,28 +16,28 @@ npm install -g gulp
 gulp
 
 # go to the dist directory and create a *new* Git repo
-cd dist
+#cd dist
 
 #create files for drploy on heroku with php
 touch index.php
-echo "<?php include_once(\"html\pages\pages.html\"); ?>" > index.php
+echo "<?php include_once(\"dist\html\pages\pages.html\"); ?>" > index.php
 touch composer.json
 echo "{}" > composer.json
 
 
-git init
+#git init
 
 #create remote to heroku
-heroku git:remote -a mridus
+#heroku git:remote -a mridus
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
-git add .
-git commit -m "Deploy to Heroku"
+#git add .
+#git commit -m "Deploy to Heroku"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-echo "git push"
-git push --force --quiet heroku master
+#echo "git push"
+#git push --force --quiet heroku master
