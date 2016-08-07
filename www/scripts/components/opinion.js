5141708ab4fbe9dae64807b154d319434fe9172c
Katherina Utlik <katherinautlik@gmail.com>
2016-08-07 21:17:02 +0300

    [Refactoring] Refactored: colors, counter block, class names, js

---
+++ www/scripts/components/opinion.js
@@ -0,0 +1,34 @@
+$(function(){
+
+	function initOpinionSwipers() {
+		$('.view_opinion').each(function () {
+			var opinionSlider = new Swiper($(this), {
+				nextButton: $(this).find('.opinion-button-next'),
+				prevButton: $(this).find('.opinion-button-prev'),
+				autoplay: 4000,
+				nested: true,
+				// observer: true,
+				resistanceRatio: 0,
+				// setWrapperSize: true,
+				autoplayDisableOnInteraction: false
+
+			});
+
+			/* текущий слайд */
+			var currentPlaceholder = $(this).find('#opinion__slideCurrent');
+			currentPlaceholder.html(opinionSlider.activeIndex + 1);
+
+			opinionSlider.on('SlideChangeEnd', function () {
+				currentPlaceholder.html(opinionSlider.activeIndex + 1);
+			});
+
+			/* сколько всего */
+			var slides = opinionSlider.slides.size();
+			$(this).find('#opinion__slideTotal').html(slides);
+
+		});
+	}
+
+	initOpinionSwipers();
+
+});
--- www/scripts/js.js
+++ www/scripts/js.js
@@ -1,42 +1,6 @@
+// Only for plugin initialization,
+// please create new js file for other purpose
+
 $(document).ready(function() {
     $('.sticky').Stickyfill();
 });
-
-$(function(){
-
-    function initOpinionSwipers() {
-        $('.view_opinion').each(function () {
-            var opinionSlider = new Swiper($(this), {
-                nextButton: $(this).find('.opinion-button-next'),
-                prevButton: $(this).find('.opinion-button-prev'),
-                autoplay: 4000,
-                nested: true,
-                // observer: true,
-                resistanceRatio: 0,
-                // setWrapperSize: true,
-                autoplayDisableOnInteraction: false
-
-            });
-
-            /* текущий слайд */
-            var currentPlaceholder = $(this).find('#opinion__slideCurrent');
-            currentPlaceholder.html(opinionSlider.activeIndex + 1);
-
-            opinionSlider.on('SlideChangeEnd', function () {
-                currentPlaceholder.html(opinionSlider.activeIndex + 1);
-            });
-
-            /* сколько всего */
-            var slides = opinionSlider.slides.size();
-            $(this).find('#opinion__slideTotal').html(slides);
-
-        });
-    }
-
-    initOpinionSwipers();
-
-});
-
-
-
-
