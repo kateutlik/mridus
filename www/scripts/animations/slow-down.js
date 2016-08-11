be46e135ccb7a21f077729b690f6d74ff6651c29
Katherina Utlik <katherinautlik@gmail.com>
2016-08-11 22:10:09 +0300

    [Main] Added image scale, fixed colors

--- www/scripts/animations/slow-down.js
+++ www/scripts/animations/slow-down.js
@@ -1,20 +1,45 @@
-$(window).scroll(function(){
-	var scrollVar = $(window).scrollTop();
+$(document).ready(function() {
+	var header = $('#header'),
+		sectionTitleTransparent = $('.section-title.transparent'),
+		scrollVar = $(window).scrollTop(),
+		slider = $('.slider'),
+		introSection = slider.find('.slider__image img');
 
-	if(scrollVar < 70) {
-		$('#header').addClass('state_scroll');
-		var opacity = (scrollVar * 2) / 100;
-		$('.section-title.transparent').css({'opacity': opacity, 'background': 'rgba(0,0,0,0)'});
-		$('.slowDown .slider').height(261 - scrollVar * 2);
-		$('.slowDown__overlay').css({'opacity': opacity});
-	} else {
-		$('#header').addClass('state_scroll');
-		$('.section-title.transparent').css({'opacity': 1, 'background': 'rgba(0,0,0,1)'});
-		$('.slowDown .slider').height(123);
-		$('.slowDown__overlay').css({'opacity': 1.38});
-	}
+	$(window).scroll(function () {
 
-	if(scrollVar === 0){
-		$('#header').removeClass('state_scroll');
+		scrollVar = $(window).scrollTop();
+
+		if (scrollVar < 215) {
+			header.addClass('state_scroll');
+			var opacity = (scrollVar * 2) / 100;
+			sectionTitleTransparent.css({'opacity': opacity, 'background': 'rgba(0,0,0,0)'});
+			window.requestAnimationFrame(animateIntro);
+		} else {
+			header.addClass('state_scroll');
+			sectionTitleTransparent.css({'opacity': 1, 'background': 'rgba(0,0,0,1)'});
+		}
+
+		if (scrollVar === 0) {
+			header.removeClass('state_scroll');
+		}
+	});
+
+
+	function animateIntro() {
+		//var introSection = $('.slider__image img');
+		var scaleValue = 1 - (scrollVar / 261).toFixed(5) * 0.4,
+			opacity = 1 - (scrollVar / 200);
+
+		//if( $(window).scrollTop() < 261) {
+		introSection.css({
+			'-moz-transform': 'scale(' + scaleValue + ') translateZ(0)',
+			'-webkit-transform': 'scale(' + scaleValue + ') translateZ(0)',
+			'-ms-transform': 'scale(' + scaleValue + ') translateZ(0)',
+			'-o-transform': 'scale(' + scaleValue + ') translateZ(0)',
+			'transform': 'scale(' + scaleValue + ') translateZ(0)'
+
+		});
+		slider.css({'opacity': opacity});
+		//}
 	}
 });
