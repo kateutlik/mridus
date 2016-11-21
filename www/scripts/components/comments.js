7f213c9e7bdb09b9933c5c0cc81f7136a4cadb9f
Ilya <Ilya_Valasiuk@epam.com>
2016-11-21 19:49:20 +0300

    fix for comments live comments

--- www/scripts/components/comments.js
+++ www/scripts/components/comments.js
@@ -2,7 +2,7 @@
 
 $(function() {
     $(document).ready(function() {
-        $(".comments > p, .comments-live > p").on('click', function(){
+        $(".comments > p, .comments-live > div").on('click', function(){
             $(this).closest('.comments, .comments-live').toggleClass("state_open");
 
             return false;
