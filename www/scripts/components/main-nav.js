d792f5801990d7125f9e948e84f91a4a495323b3
Ilya <Ilya_Valasiuk@epam.com>
2017-01-25 15:22:19 +0300

    fix commetns and restore scroll top

--- www/scripts/components/main-nav.js
+++ www/scripts/components/main-nav.js
@@ -6,6 +6,8 @@ $(function() {
 			burgerButton = $('.burgerButton'),
 			body = $('body');
 
+		var scrollTop;
+
 		burgerButton.click(function(e){
 			e.stopPropagation();
 			e.preventDefault();
@@ -15,6 +17,7 @@ $(function() {
 
 		$('.header__loginButton').on('click', function() {
 			body.removeClass('state_open');
+			scrollTop = document.body.scrollTop;
 			body.addClass('state_open_registration');
 			$('.registration').tabs({
                 active: 1
@@ -24,16 +27,19 @@ $(function() {
 
 		$('.header__readLater').on('click', function() {
 			body.removeClass('state_open');
+			scrollTop = document.body.scrollTop;
 			body.addClass('state_open_saved');
 			return false;
 		});
 
 		$('.registartion__wrapper .filter__close').on('click', function() {
 			body.removeClass('state_open_registration');
+			document.body.scrollTop = scrollTop;
 		});
 
 		$('.saved__wrapper .filter__close').on('click', function() {
 			body.removeClass('state_open_saved');
+			document.body.scrollTop = scrollTop;
 		});
 	});
 });
