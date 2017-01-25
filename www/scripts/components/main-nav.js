31efc07012206d06be22183f7b20a2d7a043ef99
Ilya <Ilya_Valasiuk@epam.com>
2017-01-25 13:38:31 +0300

    modals

--- www/scripts/components/main-nav.js
+++ www/scripts/components/main-nav.js
@@ -12,5 +12,28 @@ $(function() {
 
 			body.toggleClass('state_open');
 		});
+
+		$('.header__loginButton').on('click', function() {
+			body.removeClass('state_open');
+			body.addClass('state_open_registration');
+			$('.registration').tabs({
+                active: 1
+            });
+			return false;
+		});
+
+		$('.header__readLater').on('click', function() {
+			body.removeClass('state_open');
+			body.addClass('state_open_saved');
+			return false;
+		});
+
+		$('.registartion__wrapper .filter__close').on('click', function() {
+			body.removeClass('state_open_registration');
+		});
+
+		$('.saved__wrapper .filter__close').on('click', function() {
+			body.removeClass('state_open_saved');
+		});
 	});
 });
